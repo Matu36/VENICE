7f4f8b2fa964089035f083b6ca997b1eba321ca6
Matu36 <matipineda85@live.com.ar>
2024-03-16 20:57:58 -0300

    responsivo

--- src/utils/Calvin.js
+++ src/utils/Calvin.js
@@ -3,40 +3,72 @@ export const calvin = [
     id: 1,
     marca: "Calvin",
     talle: "XL",
-    imagen: "",
-    imagen2: "",
-    Cantidad: "",
-    precio: 8000,
-    codigo: "ABC123",
+    imagen:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630158/VENICE/CALVINFRONTBLANCA_k4uqgv.jpg",
+    imagen2:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630144/VENICE/CALVINBACKBLANCA_ewbku2.jpg",
+    Cantidad: 1,
+    precio: 42000,
+    codigo: "CK041",
   },
   {
     id: 2,
     marca: "Calvin",
     talle: "XL",
-    imagen: "",
-    imagen2: "",
-    Cantidad: "",
-    precio: 9000,
-    codigo: "ABC123",
+    imagen:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630150/VENICE/CALVINBLACKFRONT_z017jr.jpg",
+    imagen2:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630148/VENICE/CALVINBLACKBACK_sv4wiv.jpg",
+    Cantidad: 1,
+    precio: 40000,
+    codigo: "CK042",
   },
   {
     id: 3,
     marca: "Calvin",
     talle: "XL",
-    imagen: "",
-    imagen2: "",
-    Cantidad: "",
-    precio: 7000,
-    codigo: "ABC123",
+    imagen:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630163/VENICE/CALVINNEGRAFRONTLOGO_bsbtmd.jpg",
+    imagen2:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630148/VENICE/CALVINBLACKBACK_sv4wiv.jpg",
+    Cantidad: 1,
+    precio: 40000,
+    codigo: "CK043",
   },
   {
     id: 4,
     marca: "Calvin",
+    talle: "L",
+    imagen:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630160/VENICE/CALVINFRONTBLANCALOGO_pr7lai.jpg",
+    imagen2:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630152/VENICE/CALVINBLANCABACKLOGO_xcpqv2.jpg",
+    Cantidad: 1,
+    precio: 40000,
+    codigo: "CK044",
+  },
+  {
+    id: 5,
+    marca: "Calvin",
     talle: "XL",
-    imagen: "",
-    imagen2: "",
-    Cantidad: "",
-    precio: 7000,
-    codigo: "ABC123",
+    imagen:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630156/VENICE/CALVINBORDADAFRENTE_namksu.jpg",
+    imagen2:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630154/VENICE/CALVINBORDADABACK_kzbtol.jpg",
+    Cantidad: 1,
+    precio: 40000,
+    codigo: "CK045",
+  },
+  {
+    id: 6,
+    marca: "Calvin",
+    talle: "L",
+    imagen:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630164/VENICE/CALVINROJAFRONT_fqmam6.jpg",
+    imagen2:
+      "https://res.cloudinary.com/dmfzplfra/image/upload/v1710630147/VENICE/CALVINBACKROJA_nhbbbq.jpg",
+    Cantidad: 1,
+    precio: 40000,
+    codigo: "CK046",
   },
 ];
