8ff93c6785221c77c98ab8453f7bdb73ba6266c8
Matu36 <matipineda85@live.com.ar>
2024-03-27 09:37:16 -0300

    gif

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,54 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Player } from "video-react";
 import "video-react/dist/video-react.css";
 
 export default function Videos() {
+  useEffect(() => {
+    const gifs = document.querySelectorAll(".video-element");
+
+    gifs.forEach((gif) => {
+      gif.addEventListener("load", () => {
+        const temporizador = setTimeout(() => {
+          gif.classList.add("paused");
+          clearTimeout(temporizador);
+        }, gif.duration * 1000);
+      });
+    });
+  }, []);
+
   return (
     <div className="video-container">
       <img
         className="full-width-image"
-        src="https://res.cloudinary.com/dmfzplfra/image/upload/v1711327019/VENICE/VIDEOS/remeras_u7zrms.jpg"
+        src="https://res.cloudinary.com/dmfzplfra/image/upload/v1711538599/VENICE/Portada/Design_9_ochsp0.png"
         alt=""
       />
       <div className="video-group">
-        <Player
+        <img
           className="video-element"
-          playsInline
-          hideControls={true}
-          autoPlay
-          muted
-          fluid
-          src="https://res.cloudinary.com/dmfzplfra/video/upload/v1711328297/VENICE/VIDEOS/nike_tgxmuk.mp4"
+          key="nike"
+          src="https://res.cloudinary.com/dmfzplfra/image/upload/v1711465588/VENICE/GIF/nike_dz170j.gif"
+          alt="NIKE"
         />
-        <Player
+        <img
           className="video-element"
-          playsInline
-          hideControls={true}
-          autoPlay
-          muted
-          fluid
-          src="https://res.cloudinary.com/dmfzplfra/video/upload/v1711290729/VENICE/VIDEOS/Designer_1_koaqbc.mp4"
+          key="columbia"
+          src="https://res.cloudinary.com/dmfzplfra/image/upload/v1711465588/VENICE/GIF/columbia_fav1gf.gif"
+          alt="COLUMBIA"
         />
       </div>
       <div className="video-group">
-        <Player
+        <img
           className="video-element"
-          playsInline
-          hideControls={true}
-          autoPlay
-          muted
-          fluid
-          src="https://res.cloudinary.com/dmfzplfra/video/upload/v1711342558/VENICE/VIDEOS/HOLLISTER_gx0nbk.mp4"
+          key="hollister"
+          src="https://res.cloudinary.com/dmfzplfra/image/upload/v1711541698/VENICE/GIF/CHILD_r1qnqw.gif"
+          alt="HOLLISTER"
         />
 
-        <Player
+        <img
           className="video-element"
-          playsInline
-          hideControls={true}
-          autoPlay
-          muted
-          fluid
-          src="https://res.cloudinary.com/dmfzplfra/video/upload/v1711328296/VENICE/VIDEOS/levis_f8yulu.mp4"
+          key="levis"
+          src="https://res.cloudinary.com/dmfzplfra/image/upload/v1711465585/VENICE/GIF/levis_ewhm23.gif"
+          alt="LEVIS"
         />
       </div>
     </div>
